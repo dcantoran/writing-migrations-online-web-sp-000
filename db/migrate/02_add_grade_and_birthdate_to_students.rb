804cf91b481a4972dd732f98b5309abc423b5ac5
David Cantoran <davidcantoran36@gmail.com>
2020-02-01 00:17:13 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++
@@ -1,8 +0,0 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
-  
-  def change
-    add_column :students, :grade, :integer
-    add_column :students, :birthday, :string
-  end 
-  
-end 
